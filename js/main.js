454e47b3486926dcae364ddb8e7f14d2ff332f80
MatteoLoddo <loddomatteo96@gmail.com>
2022-05-30 18:55:58 +0200

    add randomnumber generato for "bomb" fix cell.addEventListener,

--- js/main.js
+++ js/main.js
@@ -13,11 +13,12 @@ function calcolaCelle (colonne,  righe){
 
     return calcolo
 }
+const containerNumberCell = []
+const containerBomb = [];
 // il simbolo "+" permette di ottenere come risultato un numero
 let risultato = + 
 
 buttonGenera.addEventListener("click" , function(){
-
     // controllo valore selettore difficolta
     const valoreDiff = difficolta.value
     
@@ -64,54 +65,78 @@ buttonGenera.addEventListener("click" , function(){
             break;
 
 
-            
-    }
+}
 
-    console.log(risultato);
+console.log(risultato);
 
     creaGriglia(risultato)
-
- })
-
+    bombGenerator(risultato)
 
 
-    let numBombGenerator = +[];
+
+// for(let i=0; containerNumberCell.length <= risultato; i++){
+    
+//     let randomNumberCell = Math.floor(Math.random()*110)
+//     if(!containerNumberCell.includes(randomNumberCell)){
+//         containerNumberCell.push(randomNumberCell)
+//     }
+    
+
+//     console.log(containerNumberCell);
+
+
+            
+//     }
+
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // creo una funzione per creare tanti div quante le celle calcolate in precedenza
 function creaGriglia (risultato){
-    containerGriglia.innerHTML="";
-    
-    for(let i=1; i <=risultato; i++){
-        // salvo il numero incrementale per stamparlo
-        
-        let text = Math.floor(Math.random()*risultato)
-        console.log(text);
-
-        numBombGenerator = Math.floor(Math.random()*risultato)
-
-        console.log(numBombGenerator);
-
-        
+        containerGriglia.innerHTML="";
+        for(let i=1; i <=risultato; i++){
         // creo la mia variabile per creare l elemento div
         let cella = document.createElement("div");
-
+        
         // appendo il testo nel mio div cella
-        cella.append(`${text}`);
+        // cella.append(`${randomNumberCell}`);
         
         // aggiungo gli stili del css al mio div cella tramite js
         cella.classList.add("cella");
-        
+        // aggiungere il dataset permette di "assegnare" un numero a cui possiamo riferirci ma l utente non lo vede
+        cella.dataset.index = i
 
+    
         cella.addEventListener ("click" , function (){
+            const  indiceCella = +cella.dataset.index
             // aggiunta una condizione in cui se l elemento cliccato e gia la classe che voglia aggiungere la va a togliere
-            if(cella.classList.contains("bg-onclick")){
-                cella.classList.remove("bg-onclick")
+            if(containerBomb.includes(indiceCella)){
+                cella.classList.add("bg-bomb")
             }else{
-                cella.classList.add("bg-onclick")
-            console.log(`hai cliccato ${text}`);
+                cella.classList.add("bg-onclick-true")
+            // console.log(`hai cliccato ${text}`);
             }
+            console.log(indiceCella );
+            
+            
 
         })
+
 
         // do una dimensione con stili inline al mio container per avere sempre la griglia delle dimensioni desiderate  calcolando la larghezza di ogni cella(richiamando la variabile css a cui ho  dato  la mia dimensione statica) per il numero delle colonne da ottenere
         if(risultato === 100){
@@ -126,7 +151,34 @@ function creaGriglia (risultato){
 
         // a fine di ogni ciclo appendo la mia cella nel mio container
         containerGriglia.append(cella)
+        
     }
+}
+// for(let i=0; i = 16; i++){
+//     const numBombGenerator = Math.floor(Math.random()*16)
+
+// if(numBombGenerator === containerText.value){
+
+//     containerBomb.push(numBombGenerator)
+
+//         }
+
+//         console.log(numBombGenerator);
+//     }
+function bombGenerator (numeroMax){
+    for(let i=0; containerBomb.length <= 16; i++){
+
+        const bombNumber  = Math.floor(Math.random()*risultato) +1
+        if(!(containerBomb.includes(bombNumber) && (containerNumberCell.includes(bombNumber)))){
+            containerBomb.push(bombNumber)
+            console.log(containerBomb);
+        }
+        
+
+
+    }
+
+    console.log(containerBomb);
 }
 
 
@@ -136,14 +188,7 @@ function creaGriglia (risultato){
 
 
 
-
-
-
-
-
-
-
-
+// CREO IL NUMERO RANDOM PER LE CASELLE
 
 
 
@@ -189,10 +234,4 @@ function creaGriglia (risultato){
 //         let risultato3 = calcolaCelle(7,7)
 
 //         risultato = risultato3
-//     }
-
-
-    
-    
-    
-
+//   }
